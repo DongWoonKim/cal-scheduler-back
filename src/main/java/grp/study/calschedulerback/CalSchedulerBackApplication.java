690f562d10f30a0ab99999bbd724d4165d0bae36
DongWoonKim <01030985843z@naver.com>
2022-11-15 14:15:50 +0900

    cal scheduler back setup

---
+++ src/main/java/grp/study/calschedulerback/CalSchedulerBackApplication.java
@@ -0,0 +1,13 @@
+package grp.study.calschedulerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalSchedulerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalSchedulerBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/grp/study/calschedulerback/CalSchedulerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package grp.study.calschedulerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalSchedulerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
